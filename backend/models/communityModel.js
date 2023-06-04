5114f53203dd2fa7110a9ee92484ec726ae01795
kunalgaurr <kunalgaur84@gmail.com>
2023-06-04 20:45:04 +0530

    Added description feature

--- backend/models/communityModel.js
+++ backend/models/communityModel.js
@@ -4,6 +4,7 @@ const communitySchema = mongoose.Schema(
   {
     name: { type: String, require: true },
     admin: { type: String, require: true },
+    description: { type: String, required: true },
     members: { type: Array, default: [] },
     posts: { type: Array, default: [] },
   },
