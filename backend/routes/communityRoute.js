7f2a341f1e45a4db66616ba0235883c825833008
kunalgaurr <kunalgaur84@gmail.com>
2023-06-04 20:51:46 +0530

    Connected routes to server

--- backend/routes/communityRoute.js
+++ backend/routes/communityRoute.js
@@ -1,4 +1,19 @@
 const express = require('express');
+const {
+  createCommunity,
+  getAllCommunity,
+  getSingleCommunity,
+  editCommunity,
+  joinCommunity,
+  deleteCommunity,
+} = require('../controller/communityController');
 const router = express.Router();
+
+router.route('/create').post(createCommunity);
+router.route('/all').get(getAllCommunity);
+router.route('/:communityId').get(getSingleCommunity);
+router.route('/:communityId').put(editCommunity);
+router.route('/:communityId/join').put(joinCommunity);
+router.route('/:communityId').delete(deleteCommunity);
 
 module.exports = router;
