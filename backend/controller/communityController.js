56997b6b8c6e8725f36d7364ac8e6d2eabc329e8
kunalgaurr <kunalgaur84@gmail.com>
2023-06-04 20:44:03 +0530

    Created Controllers

---
+++ backend/controller/communityController.js
@@ -0,0 +1,76 @@
+const User = require('../models/userModel');
+const Community = require('../models/communityModel');
+const communityPost = require('../models/communityPostModel');
+const tryCatch = require('');
+const errorCatcher = require('../utilites/errorCatcher');
+const AppError = require('../middleware/AppError');
+
+exports.createCommunity = errorCatcher(async (req, res) => {
+  const { admin, name, description } = req.body;
+  const user = await User.findById(admin);
+  if (!user) {
+    throw new AppError('User not found', 404);
+  }
+  const newCommunity = await Community.create({
+    admin,
+    name,
+    description,
+  });
+  newCommunity.members.push(admin);
+  await newCommunity.save();
+  return res.status(200).json(newCommunity);
+});
+
+exports.getAllCommunity = errorCatcher(async (req, res) => {
+  const communities = await Community.find();
+  return res.status(200).json(communities);
+});
+
+exports.getSingleCommunity = errorCatcher(async (req, res) => {
+  const community = await Community.findById(req.params.communityId);
+  if (!community) {
+    throw new AppError('Community nout found', 404);
+  }
+  return res.status(200).json(community);
+});
+
+exports.editCommunity = errorCatcher(async (req, res) => {
+  const community = await Community.findById(req.params.communityId);
+  if (!community) {
+    throw new AppError('Community nout found', 404);
+  }
+  const updatedData = req.body;
+  const updatedCommunity = community.updateOne({ $set: updatedData });
+  return res.status(200).json({
+    success: true,
+    message: `${community.name} updated successfully`,
+    updatedCommunity,
+  });
+});
+
+exports.joinCommunity = errorCatcher(async (req, res) => {
+  const community = await Community.findById(req.params.communityId);
+  if (!community) {
+    throw new AppError('Community not found', 404);
+  }
+  const user = await User.findById(req.body.userId);
+  if (!user) {
+    throw new AppError('User not found', 404);
+  }
+  if (community.members.includes(user._id)) {
+    community.members.pull(user._id);
+    return res
+      .status(200)
+      .json({ success: true, message: `You left ${community.name}` });
+  } else {
+    community.members.push(user._id);
+    return res
+      .status(200)
+      .json({ success: true, message: `You Joined ${community.name}` });
+  }
+});
+
+exports.deleteCommunity = errorCatcher(async (req, res) => {
+  const community = await Community.findByIdAndDelete(req.params.communityId);
+  const communityPost = await CommunityPost.findByIdAndDelete({ communityId });
+});
