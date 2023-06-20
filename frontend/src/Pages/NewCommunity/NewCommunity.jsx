0aab30ab1fa13638a482d9c376a71fc657fdaa51
kunalgaurr <kunalgaur84@gmail.com>
2023-06-20 17:34:43 +0530

    Updated new community UI

--- frontend/src/Pages/NewCommunity/NewCommunity.jsx
+++ frontend/src/Pages/NewCommunity/NewCommunity.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Wrapper } from '../../Components/Wrapper/Wrapper';
 import { Topbar } from '../../Components/Topbar/Topbar';
 import { PageWrapper } from '../../Components/PageWrapper/PageWrapper';
+import { BsFillPencilFill } from 'react-icons/bs';
 import './NewCommunity.css';
 
 export const NewCommunity = () => {
@@ -10,33 +11,37 @@ export const NewCommunity = () => {
       <Topbar />
       <PageWrapper>
         <div id="new-community-container">
-          <div id="new-community-top">
-            <img
-              src="/assets/add-image.png"
-              alt=""
-              id="new-community-cover-image"
-            />
-            <div id="new-community-top-wrapper">
-              <img
-                src="/assets/image.png"
-                alt=""
-                id="new-community-profile-image"
-              />
-              <label htmlFor="name">
-                <span className="new-community-label">
-                  Enter Community Name:
-                </span>
-                <input type="text" name="name" id="" />
-              </label>
+          <img src="/assets/bg.jpg" alt="" id="new-community-background" />
+          <div id="new-community-wrapper">
+            <div id="new-community-top">
+              <div id="new-community-image-container">
+                <img
+                  src="/assets/teamwork.png"
+                  alt=""
+                  id="new-community-image"
+                />
+                <div className="edit-image">
+                  <BsFillPencilFill />
+                </div>
+              </div>
+              <div id="input-top-right">
+                <input
+                  type="text"
+                  id="community-name-input"
+                  placeholder="Enter community name"
+                />
+                <textarea
+                  name=""
+                  id=""
+                  cols="30"
+                  rows="10"
+                  placeholder="Enter community description."
+                ></textarea>
+              </div>
             </div>
           </div>
-          <div id="new-community-details">
-            <label htmlFor="">
-              <span className="new-community-label">
-                Write something about the community:
-              </span>
-              <textarea name="" id="" cols="30" rows="10"></textarea>
-            </label>
+          <div id="new-community-button">
+            <button>Create Community</button>
           </div>
         </div>
       </PageWrapper>
