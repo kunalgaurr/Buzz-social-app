5da05cb75efe1f156fb79b830f48ba318192082f
kunalgaurr <kunalgaur84@gmail.com>
2023-06-20 17:34:17 +0530

    Added friend list dom

--- frontend/src/Components/FriendList/FriendList.jsx
+++ frontend/src/Components/FriendList/FriendList.jsx
@@ -1,8 +1,11 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
+import './FriendList.css';
+import { useNavigate } from 'react-router-dom';
 
 export const FriendList = () => {
+  const navigate = useNavigate();
   const user = useSelector((state) => state.auth.user);
   const [friends, setFriends] = useState([]);
   useEffect(() => {
@@ -12,16 +15,26 @@ export const FriendList = () => {
     };
 
     getFriends();
-  });
-  console.log(user._id);
-  console.log(user);
+  }, [user]);
   return (
     <div id="friend-list-container">
-      <span id="friend-list-title">Here are all your friends.</span>
+      <span id="chat-heading">Your friends.</span>
+      <div id="friend-list-search-bar">
+        <input
+          type="text"
+          id="friend-list-search-input"
+          placeholder="Search for your friends"
+        />
+      </div>
       {friends.map((friend) => (
-        <div id="friend-account">
-          <img src={user.image} alt="" id="friend-image" />
-          <div id="friend-right">{user.name}</div>
+        <div
+          id="friend-account"
+          onClick={() => navigate(`/chat/${friend._id}`)}
+        >
+          <img src={friend?.image} alt="" id="friend-image" />
+          <div id="friend-right">
+            <span id="friend-name">{friend?.name}</span>
+          </div>
         </div>
       ))}
     </div>
