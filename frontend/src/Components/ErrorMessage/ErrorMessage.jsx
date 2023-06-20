9eaaa5c667748d25eec5d4d633922351c97759ac
kunalgaurr <kunalgaur84@gmail.com>
2023-06-20 11:18:26 +0530

    Minor bug fix

--- frontend/src/Components/ErrorMessage/ErrorMessage.jsx
+++ frontend/src/Components/ErrorMessage/ErrorMessage.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ErrorMessage.css';
 
 export const ErrorMessage = ({ error }) => {
+  const [toggle, setToggle] = useState(false);
+
+  if (error) {
+    setToggle(true);
+    setTimeout(() => {
+      setToggle(false);
+    }, 3000);
+  }
+
   return (
     <div
       id="error-wrapper"
-      style={{ display: error === undefined ? 'none' : 'block' }}
+      style={{ display: error && toggle ? 'block' : 'None' }}
     >
       <span id="error-message">{`Error: ${error}`}</span>
     </div>
