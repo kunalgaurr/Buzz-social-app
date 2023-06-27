f586c570527841d889ae8f7b7c7cfd6f7182ab76
kunalgaurr <kunalgaur84@gmail.com>
2023-06-27 18:56:26 +0530

    Added mobile friendly components

--- frontend/src/Components/PageWrapper/PageWrapper.jsx
+++ frontend/src/Components/PageWrapper/PageWrapper.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Rightbar } from '../Rightbar/Rightbar';
-import './PageWrapper.css'
+import './PageWrapper.css';
+import { Bottombar } from '../Bottombar/Bottombar';
 
 export const PageWrapper = ({ children }) => {
   return (
     <div id="page-wrapper-container">
-      <div id="main-page">{children}</div>
+      <div id="main-page">
+        {children}
+        <Bottombar />
+      </div>
       <div id="fised-component">
         <Rightbar />
       </div>
